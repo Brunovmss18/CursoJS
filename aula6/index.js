5c5baf7e0d51b2e5188dedc68088e95fa8d74aab
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-06-14 09:13:06 -0300

    Create index.js

---
+++ aula6/index.js
@@ -0,0 +1,23 @@
+let nome = 'João'; // String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+
+let Nome; // Declarou a variável
+Nome = 'João'; // Inicializando a variável
+console.log(Nome);
+Nome = 'Otávio';
+console.log(Nome);
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilize var, utilize let.
