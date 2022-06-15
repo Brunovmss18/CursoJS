f98e502eec4254583078961a02445dabafd6f7d1
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-06-15 13:23:22 -0300

    Create index.js

---
+++ aula7/index.js
@@ -0,0 +1,17 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// Não utilize var, utilize const.
+const nome = 'João';
+console.log(nome);
+// +, -, * e /
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+console.log(resultado);
+console.log(resultadoDuplicado);
