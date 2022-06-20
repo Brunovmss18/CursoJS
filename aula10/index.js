db017c7e55f1719b0a115f3c715ed8a7a9cb1201
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-06-20 14:42:01 -0300

    Create index.js

---
+++ aula10/index.js
@@ -0,0 +1,8 @@
+const nome = 'Luiz'; //String
+const nome1 = "Luiz"; //String
+const nome2 = `Luiz`; //String
+const num1 = 10; //Number
+const num2 = 10.52; //Number
+let nomeAluno; // undefined -> não aponta pra local nenhum na memória
+let sobrenomeAluno = null; //Nulo -> não a pra local nenhum na memória
+const boolean = True; //true, false (lógico)
