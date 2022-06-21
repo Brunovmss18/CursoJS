5ef42b2af98fcee743482a2de8f93900c96280a4
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-06-21 10:31:11 -0300

    Create index.js

---
+++ aula11/index.js
@@ -0,0 +1,46 @@
+/*
+* Aritméticos
+* + Adição / concatenação
+* - / *
+* ** potência
+* % resto da divisão
+*/ 
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+/*
+* Ordem de Procedência
+* ()
+* **
+* * / %
+* + -
+*/
+
+/*
+operador de incremento:
+let contador = 1;
+contador++;
+ou
+let contador;
+let passo = 2;
+contador += passo;
+operador de decremento:
+let contador = 1;
+contador--;
+ou
+let contador;
+let passo = 2;
+contador -= passo;
+OBS: Também pode se usar *, **, / e %
+*/
+
+/*
+Atribuição
+// NaN - Not a Number, parseInt(inteiro), parseFloat(decimais)
+OBS: Sempre que possível o JavaScript vai tentar converter uma String para um Number.
+*/
+const Num1 = 10;
+const Num2 = Number('5.2');
+console.log(Num1 + Num2);
+
+ 
