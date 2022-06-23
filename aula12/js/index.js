73dbb0694a09cd359f7978e638f57bf465c08da0
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-06-23 15:42:34 -0300

    aula13

--- aula12/js/index.js
+++ aula12/js/index.js
@@ -1,4 +1,3 @@
-const num1 = window.prompt('Digite um número:');
-const num2 = window.prompt('Digite outro número:');
-const resultado = Number(num1) + Number(num2)
-window.alert(`A soma do primeiro número mais o segundo número é ${resultado}`);
+const num1 = Number(window.prompt('Digite um número:'));
+const num2 = Number(window.prompt('Digite outro número:'));
+window.alert(`A soma do primeiro número mais o segundo número é ${num1 + num2}`);
