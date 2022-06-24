44f57f94432a25589357efc9b530e39c03a82de7
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-06-24 09:25:16 -0300

    criei aula14

---
+++ aula14/index.js
@@ -0,0 +1,10 @@
+let umaString = 'um texto';
+console.log(umaString.charAt(6));
+console.log(umaString[4]);
+console.log(umaString.indexOf('o', 3));
+console.log(umaString.lastIndexOf('m', 3));
+console.log(umaString.length);
+console.log(umaString.slice(3, 8));
+console.log(umaString.split(' '));
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
