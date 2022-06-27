ff3a0cda9e5a3704753837b9250ae0cbf72e663d
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-06-27 15:14:33 -0300

    criei aula 15

---
+++ aula15/index.js
@@ -0,0 +1,14 @@
+let num1 = 1; //number
+let num2 = 2.5; //number
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString();
+//console.log(num1.tofixed(2));
+//console.log(num1.isinteger(num1));
+//let temp = num1 * 'ola';
+//console (temp);
+//console.log(Number.isaNaN(temp));
+//num1 += num2; //0.8
+//num1 += num2; //0.9
+//num1 += num2; //1.0
+//num1 = parsefloat(num1.tofixed(2));
