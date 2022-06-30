028ba8208c53e2c6998e50b0ebe729c56dd49797
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-06-30 09:05:04 -0300

    criei aula 17

---
+++ aula17/index.js
@@ -0,0 +1,15 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+//console.log(alunos);
+//console.log(alunos[0]);
+
+//alunos[0] = 'Eduardo';
+//console.log(alunos);
+//aluno[3] = 'Luiza';
+//console.log(alunos);
+
+//alunos[alunos.length] = 'Luiza'; // Adiciona ao final
+//alunos.push('Luiza'); // Adicional ao final de um array
+//alunos.unshift('Luiza'); // Adiciona no começo de um array
+//alunos.pop(); // Remove o último elemento de um array
+//alunos.shift(); // Remove o primeiro elemento de um array
+//delete alunos[1];
