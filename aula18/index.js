0a964bbed14a0b82f14ab53bc29bb9b341918903
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-07-01 15:02:55 -0300

    criei aula 18

---
+++ aula18/index.js
@@ -0,0 +1,16 @@
+/*function saudacao(nome) {
+    return `Bom dia ${nome}!`;
+}
+const variavel = saudacao('Bruno');
+console.log(variavel);*/
+
+/*function soma(x=1, y=1) {
+    const resultado = x + y;
+    return resultado
+}
+console.log(soma(2, 2));*/
+
+const raiz = function (n) {
+    return n ** 0.5
+};
+console.log(raiz(9));
