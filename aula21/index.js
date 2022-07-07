4c1a4540fc74840372fd75bfe99d28c9889e3c42
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-07-07 18:20:59 -0300

    atualizei a aula 21

--- aula21/index.js
+++ aula21/index.js
@@ -1,6 +1,15 @@
 function clicar() {
-    let nome = document.getElementById("Nome").value;
-    let sobreNome = document.getElementById("Sobrenome").value;
-    let peso = Number(document.getElementById("Peso").value);
-    let altura = Number(document.getElementById("Altura").value);
+    const form = document.querySelector("#form");
+    const res = document.querySelector("#res");
+    const pessoas = [];
+    form.onsubmit = function (evento) {
+        evento.preventDefault();
+        const nome = form.querySelector("#Nome").value;
+        const sobreNome = form.querySelector("#Sobrenome").value;
+        const peso = form.querySelector("#Peso").value;
+        const altura = form.querySelector("#Altura").value;
+        pessoas.push({nome: nome, sobrenome: sobreNome, peso: peso, altura: altura});
+        console.log(pessoas);
+        res.innerHTML += `${nome} ${sobreNome} ${peso} ${altura} <br />`;
+    }
 }
