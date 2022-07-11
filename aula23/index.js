566f464f60e85b1a4e56e18bf4cb448739f4d4f7
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-07-11 08:54:03 -0300

    criei aula 23

---
+++ aula23/index.js
@@ -0,0 +1,9 @@
+/*
+operadores lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+console.log(true && true);
+console.log(true || false);
+console.log(!false);
