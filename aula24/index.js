68749bfdac53349318f13abfeed7470d6a7feabd
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-07-12 09:10:43 -0300

    criei aula 24

---
+++ aula24/index.js
@@ -0,0 +1,20 @@
+/*
+&& -> false && true -> false  "o valor mesmo"
+|| -> true || false -> vai retornar "o valor verdadeiro"
+
+falsy
+*false
+0
+'' "" ``
+null / undefined
+NaN
+*/
+console.log('Luiz' && 'Maria');
+console.log(0 || false || null || 'Luiz Otávio' || true);
+
+const a = 0;
+const b = null;
+const c = 'false'
+const d = false;
+const e = NaN;
+console.log(a || b || c || d || e);
