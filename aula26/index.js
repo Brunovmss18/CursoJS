47ee686f78071d8003d09ec6ad85799dc42cdf38
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-08-10 15:44:48 -0300

    Create index.js

---
+++ aula26/index.js
@@ -0,0 +1,6 @@
+const numero = 10;
+if (numero >= 0 && numero <= 5) {
+    console.log('o numero esta entre 0 e 5.');
+} else if (numero >= 6 && numero <=8) {
+    console.log('O Numero esta entre 6 e 8');
+}
