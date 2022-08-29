28477b922ab61df848ea8dd88e5782a0855f9aa5
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-08-29 20:54:08 -0300

    Update main.js

--- aula31/modelo/assets/js/main.js
+++ aula31/modelo/assets/js/main.js
@@ -2,52 +2,52 @@ const resultado = document.getElementById('resultado');
 
 function semana(diaSemana) {
     let diaSemanaTexto;
-switch (diaSemana) {
-case 0:
-    return diaSemanaTexto = 'Domingo';
-case 1:
-    return diaSemanaTexto = 'Segunda-feira';
-case 2:
-    return diaSemanaTexto = 'Terça-feira';
-case 3: 
-    return diaSemanaTexto = 'Quarta-feira';
-case 4:
-    return diaSemanaTexto = 'Quinta-feira';
-case 5:
-    return diaSemanaTexto = 'Sexta-feira';
-case 6:
-    return diaSemanaTexto = 'Sabado';    
-}
+    switch (diaSemana) {
+    case 0:
+        return diaSemanaTexto = 'Domingo';
+    case 1:
+        return diaSemanaTexto = 'Segunda-feira';
+    case 2:
+        return diaSemanaTexto = 'Terça-feira';
+    case 3: 
+        return diaSemanaTexto = 'Quarta-feira';
+    case 4:
+        return diaSemanaTexto = 'Quinta-feira';
+    case 5:
+        return diaSemanaTexto = 'Sexta-feira';
+    case 6:
+        return diaSemanaTexto = 'Sabado';    
+    }
 }
 
 function mes(nomeMes) {
     let nomeMesTexto;
-switch (nomeMes) {
-case 0:
-    return nomeMesTexto = 'Janeiro';
-case 1:
-    return nomeMesTexto = 'Fevereiro';
-case 2:
-    return nomeMesTexto = 'Março';
-case 3:
-    return nomeMesTexto = 'Abril';
-case 4:
-    return nomeMesTexto = 'Maio';
-case 5:
-    return nomeMesTexto = 'Junho';
-case 6:
-    return nomeMesTexto = 'Julho';
-case 7:
-    return nomeMesTexto = 'Agosto';
-case 8:
-    return nomeMesTexto = 'Setembro';
-case 9:
-    return nomeMesTexto = 'Outubro';
-case 10:
-    return nomeMesTexto = 'Novembro';
-case 11:
-    return nomeMesTexto = 'Dezembro';                                   
-}    
+    switch (nomeMes) {
+    case 0:
+        return nomeMesTexto = 'Janeiro';
+    case 1:
+        return nomeMesTexto = 'Fevereiro';
+    case 2:
+        return nomeMesTexto = 'Março';
+    case 3:
+        return nomeMesTexto = 'Abril';
+    case 4:
+        return nomeMesTexto = 'Maio';
+    case 5:
+        return nomeMesTexto = 'Junho';
+    case 6:
+        return nomeMesTexto = 'Julho';
+    case 7:
+        return nomeMesTexto = 'Agosto';
+    case 8:
+        return nomeMesTexto = 'Setembro';
+    case 9:
+        return nomeMesTexto = 'Outubro';
+    case 10:
+        return nomeMesTexto = 'Novembro';
+    case 11:
+        return nomeMesTexto = 'Dezembro';                                   
+    }    
 }
 
 const data = new Date();
