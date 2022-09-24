958afc6ff2fc77f4756ce464e1e4931ef860056f
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-09-24 20:44:38 -0300

    Update main.js

--- aula36/modelo/assets/js/main.js
+++ aula36/modelo/assets/js/main.js
@@ -1,13 +1,17 @@
-const classe = window.document.getElementsByClassName('container');
+(function (){
+    const elementos = [
+        {tag: 'p', texto: 'frase 1'},
+        {tag: 'div', texto: 'frase 2'},
+        {tag: 'footer', texto: 'frase 3'},
+        {tag: 'section', texto: 'frase 4'},
+    ];
 
-const elementos = [
-    {tag: 'p', texto: 'frase 1'},
-    {tag: 'div', texto: 'frase 2'},
-    {tag: 'footer', texto: 'frase 3'},
-    {tag: 'section', texto: 'frase 4'},
-];
-window.document.createElement()
-for (let i = 0; i < elementos.length; i++) {
-    let tague = classe.document.createElement(elementos[i].tag);
-    tague.style.innerHTML = `${elementos[i].texto}` 
-}
+    const classe = window.document.querySelector('.container');
+    let tgHtml = '';
+
+    for (let i = 0; i < elementos.length; i++) {
+        let {tag, texto} = elementos[i];
+        tgHtml += `<${tag}>${texto}</${tag}>`;   
+    }
+    classe.innerHTML += `<div>${tgHtml}</div>`;
+}) ()
