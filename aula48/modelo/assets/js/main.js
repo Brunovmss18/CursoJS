331afd89b6a16ad1924c476e4dd3560531cdac24
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-10-15 09:55:47 -0300

    Update main.js

--- aula48/modelo/assets/js/main.js
+++ aula48/modelo/assets/js/main.js
@@ -3,8 +3,21 @@ const iniciar = document.querySelector('#iniciar');
 const pausar = document.querySelector('#pausar');
 const zerar = document.querySelector('#zerar');
 
-iniciar.addEventListener('click', function(event) {
-    alert('cliquei no iniciar');
+function mostraHora() {
+    const data = new Date('01-01-1970 00:00:00');
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
+
+iniciar.addEventListener('click', function() {
+    setInterval(function() {
+        relogio.innerHTML = `${mostraHora()}`;
+    }, 1000)
 });
 
 pausar.addEventListener('click', function(event) {
@@ -14,3 +27,5 @@ pausar.addEventListener('click', function(event) {
 zerar.addEventListener('click', function(event) {
     alert('cliquei no zerar');
 });
+
+
