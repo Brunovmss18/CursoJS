659d23f38e0990ec41146094618edc2d4d2eed77
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-10-21 18:57:17 -0300

    Update main.js

--- aula49/modelo/assets/js/main.js
+++ aula49/modelo/assets/js/main.js
@@ -14,13 +14,37 @@ inputTarefa.addEventListener('keypress', function(event) {
     }
 });
 
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerHTML += ' '
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerHTML = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+    li.appendChild(botaoApagar);
+}
+
 function criaTarefa(textInput) {
     const li = criaLi();
     li.innerHTML = textInput;
     tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
 }
 
 btnTarefa.addEventListener('click', function(event) {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
 });
+
+document.addEventListener('click', function(event) {
+    const el = event.target;
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
+})
