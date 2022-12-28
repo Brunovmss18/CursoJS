7f5df24e0832d5a85165921eb2995acf3866dfdf
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-12-28 16:23:12 -0300

    aula58

---
+++ aula58/modelo/assets/js/main.js
@@ -0,0 +1,11 @@
+function criaCalculadora() {
+    return {
+
+        inicia() {
+            alert('iniciei')
+        }
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
