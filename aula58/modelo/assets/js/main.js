2694fa4602a559091848c48b21d0cddc92ecb80d
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2022-12-29 20:23:19 -0300

    aula58

--- aula58/modelo/assets/js/main.js
+++ aula58/modelo/assets/js/main.js
@@ -1,9 +1,27 @@
 function criaCalculadora() {
     return {
+        display: document.querySelector('.display'),
+
+
 
         inicia() {
-            alert('iniciei')
+            this.cliqueBotoes();
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', function(e) {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+            }.bind(this));
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
         }
+
     };
 }
 
