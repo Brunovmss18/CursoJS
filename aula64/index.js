6bbe1b0c590016309c48c631f5b7d6afd327e270
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2023-01-16 13:17:32 -0300

    Create index.js

---
+++ aula64/index.js
@@ -0,0 +1,12 @@
+const nomes = ['bruno', 'vinicios', 'morais', 'santos'];
+
+//unshift
+nomes.splice(2, 0, 'thiago', 'camilo');
+console.log(nomes);
+
+//nomes.splice(índice atual, delete, elen1, elen2, elen3);
+//pop
+//const removidos = nomes.splice(-1, 1);
+
+//shift
+//const removidos = nomes.splice(0, 1);
