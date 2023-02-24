05314d69ded21689a988fb2b47f7301e5b289dce
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2023-02-24 15:13:32 -0300

    Create index.js

---
+++ aula77/index.js
@@ -0,0 +1,49 @@
+//Produto -> aumento, desconto
+//Camiseta = cor, Caneca = material
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+Produto.prototype.aumento = function(quantia) {
+    this.preco += quantia;
+};
+Produto.prototype.desconto = function(quantia) {
+    this.preco -= quantia
+};
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+    this.cor = cor;
+}
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: true,
+        get: function() {
+            return estoque;
+        },
+        set: function(valor) {
+            if(typeof valor !== 'number') return;
+            estoque = valor;
+        }
+    });
+}
+
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+const produto = new Produto('Gen', 111);
+const camiseta = new Camiseta('Regata', 7.5, 'preta');
+const caneca = new Caneca('Caneca', 13, 'Cêramica', 5);
+
+console.log(caneca);
+console.log(produto);
+camiseta.aumento(10);
+console.log(camiseta);
