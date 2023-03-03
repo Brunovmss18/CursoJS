babc4b82800f2950cd740c3dd0f90bce7b32e385
Brunovmss18 <97049936+Brunovmss18@users.noreply.github.com>
2023-03-03 14:16:45 -0300

    Update index.js

--- aula78/index.js
+++ aula78/index.js
@@ -1,6 +1,3 @@
 function ValidaCPF(cpfEnviado) {
-    const cpfLimpo = cpfEnviado.replace(/\D+/g, '');
-    const cpfArray = Array.from(cpfLimpo);
-    return console.log(cpfArray);
+    
 }
-ValidaCPF('712.718.244-26');
